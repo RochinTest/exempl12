3a12fd4f7c9f0ea9a02fc18983ceea82e164c117
Rimlin <Rimlin@gmail.com>
2018-12-25 16:42:35 +0200

    moy

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int m = 0, n = 0,p=0;
+        m1:
+        {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Введите пароль вида ****");
+                if (sc.hasNextInt())
+                    m = sc.nextInt();
+                n = 2345;
+                if (m != n) System.out.println("Пароль не верный");
+                else break m1;
+            }}
+
+        System.out.println("входите");
+
+
+    }
+}
